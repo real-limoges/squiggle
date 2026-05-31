d2b4c63b6fa07ab80bc669ade05e9b997b0ffbf4
Real Limoges <b.real.limoges@gmail.com>
2026-05-31 11:41:47 -0700

    update preprocessing of book

--- scripts/preprocess-corpus.lisp
+++ scripts/preprocess-corpus.lisp
@@ -3,6 +3,9 @@
 (defun count-sentences (paragraph)
   (count-if (lambda (c) (member c '(#\. #\! #\?))) paragraph))
 
+(defun valid-paragraph? (text)
+    (<= 3 (count-sentences text) 20))
+
 (defun split-on-blank-lines (text)
   (let ((paragraphs nil)
         (current nil))
@@ -16,8 +19,15 @@
             (push (str:join " " (nreverse current)) paragraphs))
           (nreverse paragraphs)))
 
-; (defun preprocess-corpus (&key (in "corpus/book.txt") (out "corpus/passages.txt"))
-;   (let* ((text (uiop:read-file-string in))
-;   )))
+(defun preprocess-corpus (&key (in "corpus/book.txt") (out "corpus/passages.txt"))
+  (let* ((text (uiop:read-file-string in))
+         (split-text (split-on-blank-lines text))
+         (filtered-text (remove-if-not #'valid-paragraph? split-text)))
+    (with-open-file (stream out :direction :output
+                                :if-exists :supersede
+                                :if-does-not-exist :create)
+      (dolist (passage filtered-text)
+        (write-line passage stream)))
+    (length filtered-text)))
 
-; (preprocess-corpus)
+(preprocess-corpus)
